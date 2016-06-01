10621ac48d7bf6215152a0ce7f1a53feba1f3c18
monsteronfire <monsteronfire@gmail.com>
2016-06-01 08:24:59 +0800

    Add properties to contact controller

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  message: '',
+  useremail: '',
+
+  isFilled: Ember.computed.gte('message', 5),
+
+  isValid: Ember.computed.and('message', 'useremail'),
+
+  isEmpty: Ember.computed('message', function() {
+    console.log(this.get('message'));
+  })
 });
