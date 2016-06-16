9e57289a46a3ec558861328a4f889bc360bd3818
monsteronfire <monsteronfire@gmail.com>
2016-06-16 11:24:29 +0800

    Update library routes

--- app/routes/libraries/index.js
+++ app/routes/libraries/index.js
@@ -2,16 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.createRecord('library');
-  },
-
-  actions: {
-    saveLibrary(newLibrary) {
-      newLibrary.save().then(() => this.transitionTo('libraries'))
-    },
-
-    willTransition() {
-      this.controller.get('model').rollbackAttributes();
-    }
+    return this.store.findAll('library');
   }
 });
