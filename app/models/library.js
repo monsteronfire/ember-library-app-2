e709e7ba82c39a2e795dcdc20a385937e6d4c2d4
monsteronfire <monsteronfire@gmail.com>
2016-06-19 18:00:55 +0800

    Generate admin/seeder route

--- app/models/library.js
+++ app/models/library.js
@@ -1,11 +1,13 @@
 import Ember from 'ember';
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+import {hasMany} from 'ember-data/relationships';
 
 export default Model.extend({
   name: attr('string'),
   address: attr('string'),
   phone: attr('string'),
+  books: hasMany('book'),
 
   isValid: Ember.computed.notEmpty('name')
 });
--- app/router.js
+++ app/router.js
@@ -11,6 +11,7 @@ Router.map(function() {
 
   this.route('admin', function() {
     this.route('invitations');
+    this.route('seeder');
   });
 
   this.route('libraries', function() {
