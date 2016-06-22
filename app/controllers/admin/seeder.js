4cadc0df8dff24cd3605872f45504c72903c85f7
monsteronfire <monsteronfire@gmail.com>
2016-06-22 23:10:41 +0800

    Add model code

---
+++ app/controllers/admin/seeder.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+import Faker from 'faker';
+
+export default Ember.Controller.extend({
+  libraries: [],
+  books: [],
+  authors: [],
+
+  actions: {
+    generateLibraries() {}
+  }
+});
--- app/models/library.js
+++ app/models/library.js
@@ -2,12 +2,25 @@ import Ember from 'ember';
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import {hasMany} from 'ember-data/relationships';
+import Faker from 'faker';
 
 export default Model.extend({
   name: attr('string'),
   address: attr('string'),
   phone: attr('string'),
-  books: hasMany('book'),
+  books: hasMany('book', {inverse: 'library', async: true}),
 
-  isValid: Ember.computed.notEmpty('name')
+  isValid: Ember.computed.notEmpty('name'),
+
+  randomize() {
+    this.set('name', Faker.company.companyName() + ' Library');
+    this.set('address', this._fullAddress());
+    this.set('phone', Faker.phone.phoneNumber());
+
+    return this;
+  },
+
+  _fullAddress() {
+    return `${Faker.address.streetAddress()}, ${Faker.address.city()}`;
+  }
 });
